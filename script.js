c4e88b048bff478e26901b757470471cf2c7e47c
malyrudy <malyrudy@gmail.com>
2022-06-04 23:27:03 +0200

    Everything EXCEPT
    sunrise and sunset are not visible. Gonna have to figure it out the code the server is broadcasting.

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ let weather = {
         const {icon, description} = data.weather[0];
         const {temp, humidity} = data.main;
         const {speed} = data.wind;
-        console.log(name, icon, description, temp, humidity, speed);
+        const {sunrise} = data.sys;
+        const {sunset} = data.sys;
+        console.log(name, icon, description, temp, humidity, speed, sunrise, sunset);
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".temp").innerText = temp + " °C";
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
@@ -24,7 +26,20 @@ let weather = {
         document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
     },
     search: function () {
-        this.fetchWeather(document.querySelector(".search bar").value);
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
 };
 
+document
+    .querySelector(".search button")
+    .addEventListener("click", function () {
+        weather.search();
+    });
+
+    document
+        .querySelector(".search-bar")
+        .addEventListener("keyup", function (event) {
+            if (event.key == "Enter") {
+                weather.search();
+            }
+        })
